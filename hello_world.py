d61b409d408c55a1a4b12b050ed41396321c1a9d
richatkr <richathakur532@gmail.com>
2022-07-14 13:45:07 +0530

    Hello world in python

---
+++ hello_world.py
@@ -0,0 +1,37 @@
+print("hello world")
+x = False
+
+if(x):
+    print("inside if")
+print("outside if")
+rent = 5000
+gas = 2000
+groceries = 4000
+total = rent + gas + groceries
+print(total)
+item1 = 'rent'
+item2 = 'gas'
+item3 = 'groceries'
+print('Item List',item1,item2,item3)
+print(10+20)
+print(20-10)
+print(10*15)
+print(10/2)
+print(11%2)
+print(2**4)
+print(10+2*3)
+print((10+2)*3)
+print(3-2.7)
+print(round(3-2.7,2))
+item = "Ice Cream"
+print(item[0])
+print(item[0:3])
+print(item[4:9])
+print(item[4:])
+print(item[:3])
+item2=['Bread', 'Butter', 'Cream', 'Milk', 'Cheese', 'Paneer', 'Sweets']
+print(item2)
+item2[1] = 'Egg'
+print(item2)
+list2=['Shampoo','Shop','Surf']
+print(item2+list2)
