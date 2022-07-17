764c2d907eb8de3397e72c719dd09202a761786f
richatkr <richathakur532@gmail.com>
2022-07-17 13:21:46 +0530

    fixed typos

--- Question_on_json/json_question_1.py
+++ Question_on_json/json_question_1.py
@@ -6,11 +6,11 @@ file_string = f.read()
 people_list_json=json.loads(file_string)
 people_list = people_list_json["address_book"]
 list_of_country={}
-for people in people_list:
-    if people['country'] in list_of_country:
-        list_of_country[people["country"]].append(people)
+for person in people_list:
+    if person['country'] in list_of_country:
+        list_of_country[person["country"]].append(person)
     else:
-        list_of_country[people["country"]]=[people]
+        list_of_country[person["country"]]=[person]
 list_of_country_json= json.dumps(list_of_country, indent=4)
 with open("/Users/richa/Desktop/python/Question_on_json/countries_list.json","w") as f:
     f.write(list_of_country_json)
